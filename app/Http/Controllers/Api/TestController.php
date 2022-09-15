420b42631003a5dd5fdf0d33230504a260a236f7
208 <2084759131@qq.com>
2022-09-15 15:35:39 +0800

    1112

---
+++ app/Http/Controllers/Api/TestController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Response;
+
+class TestController extends BaseController
+{
+    public function phone(Request $request)
+    {
+        $row = DB::table('Test')->where('ID', $request->ID)->first();
+        if ($row && $row->Num > 0) {
+            $num = $row->Num - 1;
+            $res = DB::update('update Test set Num = ' . $num . ' where ID = ' . $row->ID . ';');
+            if ($res) {
+                return Response::json(array(
+                    'Code' => 200,
+                    'Status' => 'SUCCESS',
+                    'Message' => $res ? '' : 'empty'
+                ));
+            }
+        }
+        return Response::json(array(
+            'Code' => 200,
+            'Status' => 'SUCCESS',
+            'Message' => 'empty'
+        ));
+    }
+}
