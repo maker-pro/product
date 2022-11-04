a3f501f0a5c4e8d4cfc8fc42b6008999748e8216
maker-pro <85552249+maker-pro@users.noreply.github.com>
2022-11-04 19:08:07 +0800

    ~~~

---
+++ app/Http/Middleware/CheckIpMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckIpMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (in_array($request->getClientIp(), [])) {
+            return $next($request);
+        }
+        return response(json_encode(array(
+            'Code' => 503,
+            'Msg' => 'Error!'
+        )), 503);
+    }
+}
