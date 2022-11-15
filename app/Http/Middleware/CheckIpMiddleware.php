3877d6ba224d4d99408dc37e9f3431f2a6e4bb27
maker-pro <85552249+maker-pro@users.noreply.github.com>
2022-11-15 19:03:53 +0800

    11-15-laravel-admin

--- app/Http/Middleware/CheckIpMiddleware.php
+++ app/Http/Middleware/CheckIpMiddleware.php
@@ -15,7 +15,7 @@ class CheckIpMiddleware
      */
     public function handle($request, Closure $next)
     {
-        if (in_array($request->getClientIp(), [])) {
+        if (in_array($request->getClientIp(), ['182.61.34.107'])) {
             return $next($request);
         }
         return response(json_encode(array(
