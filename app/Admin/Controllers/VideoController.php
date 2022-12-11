b2ee44ea7c0e65f05d953b7cce04430d5fc8a848
maker-pro <85552249+maker-pro@users.noreply.github.com>
2022-12-11 17:45:11 +0800

    delete-model

--- app/Admin/Controllers/VideoController.php
+++ app/Admin/Controllers/VideoController.php
@@ -27,62 +27,61 @@ class VideoController extends AdminController
             return new Table(['Title', 'Path'], $collections->toArray(), ['list-table']);
         });
 
-        Admin::css('//vjs.zencdn.net/7.10.2/video-js.min.css');
-        Admin::style(' .list-table > tbody > tr{cursor: pointer;}');
+//        Admin::css('//vjs.zencdn.net/7.10.2/video-js.min.css');
+//        Admin::style(' .list-table > tbody > tr{cursor: pointer;}');
+//
+//        Admin::js('//vjs.zencdn.net/7.10.2/video.min.js');
+//        $script = <<<EOF
+//            var player = null;
+//            let elements = $('.list-table > tbody > tr');
+//            for (var i= 0; i < elements.length; i++) {
+//                $(elements[i]).attr('data-target', '#video-model')
+//                $(elements[i]).attr('data-toggle', 'modal')
+//            };
+//            $('.list-table > tbody > tr').click(function() {
+//                let video_path = $(this).find('td')[1].textContent;
+//                $('#source').attr('src', video_path);
+//                $('#source').attr('type', 'application/x-mpegURL');
+//                player = videojs('myVideo', {
+//                    bigPlayButton: true,
+//                    textTrackDisplay: false,
+//                    posterImage: false,
+//                    errorDisplay: false,
+//                    aspectRatio: "16:9"
+//                });
+//                player.on('loadeddata', () => {
+//                    // 先设置静音
+//                    player.muted(true);
+//                    // 再执行播放
+//                    player.play();
+//                });
+//                $('#video-model').on('hide.bs.modal', function (e) {
+//                    player = null;
+//                })
+//            });
+//            $('body').append($(`
+//                <div class="modal fade" tabindex="-1" role="dialog" id="video-model">
+//                  <div class="modal-dialog" role="document">
+//                    <div class="modal-content">
+//                      <div class="modal-header">
+//                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+//                        <h4 class="modal-title">Video Play</h4>
+//                      </div>
+//                      <div class="modal-body" style="width: 100%; height: 300px;">
+//                        <video id="myVideo" class="video-js vjs-default-skin vjs-big-play-centered" controls autoplay style="width: 100%; height: 100%;" >
+//                            <source id="source"></source>
+//                        </video>
+//                      </div>
+//                      <div class="modal-footer">
+//                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+//                      </div>
+//                    </div><!-- /.modal-content -->
+//                  </div><!-- /.modal-dialog -->
+//                </div><!-- /.modal -->
+//            `));
+//EOF;
 
-        Admin::js('//vjs.zencdn.net/7.10.2/video.min.js');
-        $script = <<<EOF
-            var player = null;
-            let elements = $('.list-table > tbody > tr');
-            for (var i= 0; i < elements.length; i++) {
-                $(elements[i]).attr('data-target', '#video-model')
-                $(elements[i]).attr('data-toggle', 'modal')
-            };
-            $('.list-table > tbody > tr').click(function() {
-                let video_path = $(this).find('td')[1].textContent;
-                $('#source').attr('src', video_path);
-                $('#source').attr('type', 'application/x-mpegURL');
-                player = videojs('myVideo', {
-                    bigPlayButton: true,
-                    textTrackDisplay: false,
-                    posterImage: false,
-                    errorDisplay: false,
-                    aspectRatio: "16:9"
-                });
-                player.on('loadeddata', () => {
-                    // 先设置静音
-                    player.muted(true);
-                    // 再执行播放
-                    player.play();
-                });
-                $('#video-model').on('hide.bs.modal', function (e) {
-                    player = null;
-                    console.log(11111111)
-                })
-            });
-            $('body').append($(`
-                <div class="modal fade" tabindex="-1" role="dialog" id="video-model">
-                  <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        <h4 class="modal-title">Video Play</h4>
-                      </div>
-                      <div class="modal-body" style="width: 100%; height: 300px;">
-                        <video id="myVideo" class="video-js vjs-default-skin vjs-big-play-centered" controls autoplay style="width: 100%; height: 100%;" >
-                            <source id="source"></source>
-                        </video>
-                      </div>
-                      <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                      </div>
-                    </div><!-- /.modal-content -->
-                  </div><!-- /.modal-dialog -->
-                </div><!-- /.modal -->
-            `));
-EOF;
-
-        Admin::script($script);
+//        Admin::script($script);
 
         $grid->column('director', __('Director'))->width(300);
         $grid->column('actor', __('Actor'))->width(300);
